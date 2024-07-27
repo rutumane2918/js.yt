4fd3f4743bd18551bfcc2de82560de6f87bd29b5
rutumane2918 <123963205+rutumane2918@users.noreply.github.com>
2024-07-27 18:18:47 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,2 @@
+const accountId = 14454
+
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,21 @@
+"use stict";// treat all js code as newer version
+
+// alert(3+3) // we are using nodejs,not browser
+
+let name = "RM" // "string data type" use "" always
+let age = 3 // "number" 
+let isLoggedIn = false // "boolean" like true and false
+
+// null => standlone value like empty value ..like having temp.0 
+// undefined = value which not aasigned
+// symbol = unique
+// object
+
+console.log(typeof null);//object
+console.log(typeof undefined); //undefined
+
+
+ 
+
+
+
