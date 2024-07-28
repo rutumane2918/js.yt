49c5f34809f92688e85d81a3726a0bff35233bb2
rutumane2918 <123963205+rutumane2918@users.noreply.github.com>
2024-07-28 07:33:10 +0000

    conversion of datatypes

---
+++ 01_basics/03_ConversionOperation.js
@@ -0,0 +1,28 @@
+let score = "33abc"               // if you put null then it gives "0",
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let  valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);      // it gives NaN (not a number) also same for undifined value 
+
+// "33" => 33
+// true => 1 ; false => 0 
+
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn =  Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1 => true ; 0 => false
+// ""=> false
+// "RM" => True
+
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
